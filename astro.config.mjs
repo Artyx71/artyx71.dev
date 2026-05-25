76755c1248032580e193fab9cae1bf34ffb68ea1
Andrew Gabaraev <androgabar@gmail.com>
2026-05-25 16:18:06 +0300

    chore(config): add GitHub Pages site/base config and Actions deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 
 export default defineConfig({
+  site: 'https://artyx71.github.io',
+  base: '/artyx71.dev',
   output: 'static',
   integrations: [
     tailwind({ applyBaseStyles: false }),
